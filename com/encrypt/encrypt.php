5e212c7fdd1ab92ae4c5e0e40f9bd09766c0793b
root <root@pal.sandbox>
2010-02-22 17:53:04 +0000

    rich's fix for encrypt

--- com/encrypt/encrypt.php
+++ com/encrypt/encrypt.php
@@ -3,10 +3,11 @@
 # ==================================================================
 function c_encrypt($input){
 
-	if($input == ""){
+	if($input[1] == ""){
 		$output = "Please provide a string to be encrypted\r<strong>encrypt string\rstring >> b45cffe084dd3d20d928bee85e7b0f21</strong>";
 	} else {
-		$output = $input . " >> " . md5($input);
+		$output = md5($input[1]);
+		///$output = $input[1] . " >> " . md5($input[1]);
 	}
 
 	return $output;
